b61394bf7747f60a19f5f0bc15a92bf394973211
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-02-09 07:29:39 +0100

    first commit

---
+++ src/main/java/de/samples/schulungen/EffectiveJavaSampleApplication.java
@@ -0,0 +1,13 @@
+package de.samples.schulungen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveJavaSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffectiveJavaSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/samples/schulungen/EffectiveJavaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.samples.schulungen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveJavaSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
